9ddd16247a417483cd03819335c17a37bc39d59b
r3nzph <abarracosorenznino@gmail.com>
2024-09-02 06:45:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -68,7 +68,8 @@ PRODUCT_PACKAGES += \
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1-service \
+    libhealthd.$(PRODUCT_PLATFORM)
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
